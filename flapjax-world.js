e15bb3a19a55a3d0a52cf2a9cb9b07ce47c9bcf4
joseph <joe@cs.brown.edu>
2012-01-07 17:35:15 -0500

    cleanup

--- flapjax-world.js
+++ flapjax-world.js
@@ -1,11 +1,19 @@
-function world(init, handlers, facets) {
-  return mergeE.apply(null,
-    handlers.map(function(handler) {
-      return handler[0].mapE(function(eventValue) {
-        return function(world) { return handler[1](world, eventValue); };
+function world(init, handlers) 
+/*: ∀ a . a * Array<∃ b . {0: EventStream<b>, 1: a * b -> a}> -> Behavior a */
+{
+  return mergeE.apply(zeroE,
+    handlers.map(function(handler)
+    /*: ∃ b . {0: EventStream<b>, 1: a * b -> a} -> EventStream<a -> a> */
+    {
+      return handler[0].mapE(function(eventValue) /*: b -> (a -> a) */ {
+        return function(world) /*: a -> a */ {
+          return handler[1](world, eventValue);
+        };
       });
     }))
-   .collectE(init, function(handler, world) { return handler(world); })
+   .collectE(init, function(handler, world) /*: (a -> a) * a -> a */ {
+      return handler(world);
+    })
    .startsWith(init);
 }
 
