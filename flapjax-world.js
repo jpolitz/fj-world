e176f51b1aacf19e9d51bf16b05c53098a36c150
joseph <joe@cs.brown.edu>
2012-01-07 16:49:34 -0500

    just return world behavior

--- flapjax-world.js
+++ flapjax-world.js
@@ -1,21 +1,11 @@
 function world(init, handlers, facets) {
-  var handlerEvents = mergeE.apply(null,
+  return mergeE.apply(null,
     handlers.map(function(handler) {
       return handler[0].mapE(function(eventValue) {
         return function(world) { return handler[1](world, eventValue); };
       });
-    })
-  );
-
-  var worldValueB = handlerEvents.collectE(
-    init,
-    function(handler, world) { return handler(world); }
-  ).startsWith(init);
-
-  var worldFacets = {};
-  Object.keys(facets).forEach(function(k) {
-    worldFacets[k] = worldValueB.liftB(facets[k]);
-  });
-  return worldFacets;
+    }))
+   .collectE(init, function(handler, world) { return handler(world); })
+   .startsWith(init);
 }
 
